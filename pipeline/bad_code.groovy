526fa4925cb18a4deae4f1dc6abd09b892e94070
mnowrouz <52008962+mnowrouz@users.noreply.github.com>
2025-04-25 12:47:36 -0400

    initial commit

---
+++ pipeline/bad_code.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Top Secret Login') {
+            steps {
+                script {
+                    // 🤫 Super confidential credentials – do NOT share (unless it's in a Jenkins log)
+                    def username = "admin"
+                    def password = "Cyberark1" // nothing says "secure" like hardcoding secrets
+
+                    // 🚨 Printing sensitive info directly to the logs because we trust everyone here
+                    echo """
+        _  
+     __|_|__                
+      (•_•)         
+     <)   )╯    
+     /   \\     Never Hardcode secrets...
+     
+     
+     ...without me ;)
+"""
+
+                    echo "Logging in as '${username}' with password: '${password}' 🔓"
+                    
+                    
+                    if (password == "Cyberark1") {
+                        echo "Access granted. Welcome to the vault 👑 lol"
+                    } else {
+                        echo "Incorrect password. Did you mean 'admin123'?"
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ pipeline/good_code.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Top Secret Login') {
+            steps {
+                script {
+                    withCredentials([
+                        conjurSecretCredential(credentialsId: 'Vault-app1-Jenkins_Demo_Automation-Operating System-AutomationUser-admin-username', variable: 'USERNAME'),
+                        conjurSecretCredential(credentialsId: 'Vault-app1-Jenkins_Demo_Automation-Operating System-AutomationUser-admin-password', variable: 'PASSWORD')
+                    ]) {
+
+                        def username = USERNAME
+                        def password = PASSWORD
+
+                        echo """
+        _  
+     __|_|__                
+      (•_•)         
+     <)   )╯    
+     /   \\     Never hardcode secrets...
+     
+     
+     ...ever ;(
+"""
+
+                        echo "Logging in as '${username}' with password: (shhh... it's a secret) 🔓"
+
+                        if (password == "Cyberark1") {               // Keeping this here to prove the masked secret = Cyberark1
+                            echo "Access granted. Welcome to the vault 👑 lol"
+                        } else {
+                            echo "Incorrect password. Did you mean 'admin123'?"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
